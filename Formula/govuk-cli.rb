8de51612dabc192892d4d1b6c51118314c0d8629
Issy Long <me@issyl0.co.uk>
2019-08-01 17:05:01 +0100

    govuk-cli: New formula for easy installation of `govuk connect` scripts
    - This is almost entirely unversioned as it's just a bunch of shell
      scripts. This is unfortunate, as `brew update` and `brew upgrade`
      won't be as easy, but depending on how many changes we
      foresee for `govuk-cli`, we can do release tagging when we need to.
    - Note that this is _not_ the `govukcli` tool from the `govuk-aws` repo.
      If someone wants this, they can package it themselves. But the dream is
      to be consistent with one tool with one name. :-)
    - This is installable with `brew install --HEAD govuk-cli` if you want
      to get the latest changes.
    
    Co-authored-by: Chris Baines <christopher.baines@digital.cabinet-office.gov.uk>

---
+++ Formula/govuk-cli.rb
@@ -0,0 +1,13 @@
+class GovukCli < Formula
+  desc "CLI for GOV.UK to help connecting to machines in the estate"
+  homepage "https://github.com/alphgov/govuk-cli.git"
+  # The repo doesn't tag versions (yet).
+  url "https://github.com/alphagov/govuk-cli.git"
+  version "0.0.1"
+  head "https://github.com/alphagov/govuk-cli.git"
+
+  def install
+    bin.install "bin/govuk-connect"
+    bin.install "bin/govuk"
+  end
+end
