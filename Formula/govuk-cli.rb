0097a7b43506739c1100e221fd2b7a7d9aa49bab
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2019-08-28 09:15:45 +0100

    Correct spelling of alphagov
    Gimme an "a".

--- Formula/govuk-cli.rb
+++ Formula/govuk-cli.rb
@@ -1,6 +1,6 @@
 class GovukCli < Formula
   desc "CLI for GOV.UK to help connecting to machines in the estate"
-  homepage "https://github.com/alphgov/govuk-cli.git"
+  homepage "https://github.com/alphagov/govuk-cli.git"
   # The repo doesn't tag versions (yet).
   url "https://github.com/alphagov/govuk-cli.git"
   version "0.0.1"
